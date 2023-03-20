966cbc1a6d0746d37137b226e09dded55ed4c797
Davyd Kuleba <kulebadavid@gmail.com>
2023-03-20 19:55:00 -0400

    Converter 1.0

--- Converter - Completed/ContentView.swift
+++ Converter - Completed/ContentView.swift
@@ -8,14 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var input = 100.0
+    @State private var inputUnit = "Meters"
+    @State private var outputUnit = "Kilometers"
+    
+    let units = ["Feet", "Kilometers", "Meters", "Miles", "Yards"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            Section{
+                TextField("Amount", value: $input, format: .number)
+                    .keyboardType(.decimalPad)
+            } header: {
+                Text("Amount to convert")
+            }
         }
-        .padding()
     }
 }
 
